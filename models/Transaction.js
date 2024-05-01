5cda37644237dfd6aab266bac228fef38e00ecc4
ODyachenko <oleg1696@gmail.com>
2024-05-01 15:48:49 +0300

    Transaction model

---
+++ models/Transaction.js
@@ -0,0 +1,36 @@
+import mongoose, { Schema } from 'mongoose';
+
+const TransactionSchema = new mongoose.Schema(
+  {
+    cardId: {
+      type: Schema.Types.ObjectId,
+      ref: 'Card',
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+    value: {
+      type: Number,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    direction: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.Schema('Transaction', TransactionSchema);
