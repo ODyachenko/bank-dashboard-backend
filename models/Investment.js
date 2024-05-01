7dcd6a3ba78f58636ff668947946902b4d378138
ODyachenko <oleg1696@gmail.com>
2024-05-01 15:52:56 +0300

    Investment model

---
+++ models/Investment.js
@@ -0,0 +1,29 @@
+import mongoose, { Schema } from 'mongoose';
+
+const InvestmentSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId(),
+      ref: 'User',
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: Number,
+      required: true,
+    },
+    returnValue: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.Schema('Investment', InvestmentSchema);
