bec01823f41bef12e2f76eb56498db953986e6f9
ODyachenko <oleg1696@gmail.com>
2024-05-01 17:10:36 +0300

    edit card validation

--- validations/card.js
+++ validations/card.js
@@ -1,12 +1,13 @@
 import { body } from 'express-validator';
 
 export const cardValidation = [
-  body('name', 'Please enter the valid card name').isCreditCard(),
+  body('name', 'Please enter the valid card name').isLength({
+    min: 4,
+  }),
   body('holder', 'Full name must be greater than 4 symbols').isLength({
     min: 4,
   }),
   body('expiredDate', 'Please enter the valid date').isDate(),
-  body('number').isNumeric().isLength(10),
+  body('number', 'Сard number must be 10 digits').isNumeric().isLength(10),
   body('cvv').isNumeric(),
-  body('').isIdentityCard,
 ];
